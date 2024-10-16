7832f8e4b1f45d79d14098bc0be38a964c5f0672
Jivan-Kajave <103193579+Jivan-Kajave@users.noreply.github.com>
2024-10-16 16:43:03 +0530

    Add files via upload

---
+++ Linux/Activity4.sh
@@ -0,0 +1,13 @@
+jivan@WIN-38DN70MO14E:~/FSTLinux$ vim activity4.sh
+jivan@WIN-38DN70MO14E:~/FSTLinux$ chmod +x activity4.sh
+jivan@WIN-38DN70MO14E:~/FSTLinux$ ./activity4.sh
+jivan@WIN-38DN70MO14E:~/FSTLinux$ ls
+Music  Pictures  Videos  activity4.sh  hello.txt
+jivan@WIN-38DN70MO14E:~/FSTLinux$ vim activity4.sh
+jivan@WIN-38DN70MO14E:~/FSTLinux$ ls Music
+song1.mp3  song2.mp3  song3.mp3  song4.mp3  song5.mp3  song6.mp3
+jivan@WIN-38DN70MO14E:~/FSTLinux$ ls Pictures
+snap1.jpg  snap2.jpg  snap3.jpg  snap4.jpg  snap5.jpg  snap6.jpg
+jivan@WIN-38DN70MO14E:~/FSTLinux$ ls Videos
+film1.mp4  film2.mp4  film3.mp4  film4.mp4  film5.mp4  film6.mp4
+jivan@WIN-38DN70MO14E:~/FSTLinux$
